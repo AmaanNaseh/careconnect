2756d7ead0dd49e35f4a926de4891907daaef911
AmaanNaseh <naseh.amaan@gmail.com>
2025-02-20 00:00:30 +0530

    added all files

---
+++ client/src/Pages/References.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const References = () => {
+  return (
+    <ul className="list-disc m-4 md:m-8 lg:m-12 flex flex-col gap-4">
+      <a
+        href="https://www.flaticon.com/free-icons/doctor-patient"
+        title="doctor patient icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li>Doctor patient icons created by Backwoods - Flaticon</li>
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/professions-and-jobs"
+        title="professions and jobs icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li>
+          Professions and jobs icons created by Prosymbols Premium - Flaticon
+        </li>
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/patient"
+        title="patient icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li> Patient icons created by Freepik - Flaticon</li>
+      </a>
+      <a
+        href="https://pngtree.com/freebackground/health-care-abstract-light-effect-icon-decoration_1591329.html"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li> Background Poster</li>
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/monitoring"
+        title="monitoring icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li> Monitoring icons created by Talha Dogar - Flaticon</li>
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/artificial-intelligence"
+        title="artificial intelligence icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <li>
+          Artificial intelligence icons created by Design Circle - Flaticon
+        </li>
+      </a>
+    </ul>
+  );
+};
+
+export default References;
